4b4971c2ef750e70ebc9c64b012788f7a70492e9
soydiego-web <davilas2@unemi.edu.ec>
2026-06-03 04:17:36 -0500

    cambio de nombre

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       {/* Fragment (<>) agrupa elementos sin agregar un div extra al DOM */}
-      <Navbar titulo="Diego.dev" />
+      <Navbar titulo="Diego" />
       <main>
         <Hero />
         <Sobre />
